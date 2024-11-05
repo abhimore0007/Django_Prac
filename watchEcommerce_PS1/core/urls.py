7c755958ba4874470ba2e8a3f5a227730768b8ce
Abhi More <moreabhi0007@gmail.com>
2024-11-05 15:39:04 +0530

    update

--- watchEcommerce_PS1/core/urls.py
+++ watchEcommerce_PS1/core/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.base,name="base"),
-    path('login/',views.login,name="login")
+    path('register/',views.register,name="register"),
+    path('login/',views.user_login,name="login"),
+    path('logout/',views.log_out, name="logout"),
+    path('Profile/',views.user_Profile, name="Profile")
 ]
--- watchEcommerce_PS1/core/views.py
+++ watchEcommerce_PS1/core/views.py
@@ -1,9 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import RegisterForm,loginForm
+from django.contrib.auth import authenticate,login,logout,update_session_auth_hash
+from django.contrib import messages
 # Create your views here.
 
 def base(request):
     return render(request,'core/index.html')
 
-def login(request):
-    return render(request,'core/login_form.html')
+def register(request):
+    if request.method == 'POST':
+        reg=RegisterForm(request.POST)
+        if reg.is_valid():
+            reg.save()
+            messages.success(request,'Registration Successfully !!')
+            return redirect('register')
+    else:
+        reg=RegisterForm()
+    return render(request,'core/register_form.html',{'reg':reg})
+
+def user_login(request):
+    if request.method == 'POST':
+        log=loginForm(request,request.POST)
+        if log.is_valid():
+            Name=log.cleaned_data['username']
+            Password=log.cleaned_data['password']
+            user=authenticate(username=Name,password=Password)
+            messages.success(request,'Login SuccessFully !!')
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+    else:
+        log=loginForm()
+        return render(request,'core/login_form.html',{'log':log})
+
+def log_out(request):
+    logout(request)
+    return redirect('base')
+
+
+def user_Profile(request):
+    return render(request,'core/profile.html')
